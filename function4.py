db9dc517036798f2c27949e5f9729688c59bd38e
fr33p13 <freedom314@hanmail.net>
2022-07-19 10:09:34 +0900

    함수연습

---
+++ function4.py
@@ -0,0 +1,37 @@
+# function4.py
+#기본값을 셋팅
+def times(a=10,b=20):
+    return a*b 
+
+#호출
+print(times())
+print(times(5))
+print(times(5,6))
+
+#가변인자(입력 파라메터 갯수가 가변적인 경우)
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result 
+
+#호출 
+print(union("HAM","EGG"))
+print(union("HAM","EGG","SPAM"))
+
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server +":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] +"&"
+    return strURL
+
+print(userURIBuilder("credu.com", "80", id="kim", password="1234"))
+print(userURIBuilder("credu.com", "80", id="kim", password="1234", name="mike"))
+
+
+g = lambda x,y:x*y
+print(g(3,4))
+print( (lambda x:x*x)(3))
+print(globals())
